331e95588d0a4f00ab5d81611374ad6b3436a1bf
Roshan <pahariroshan55@gmail.com>
2026-06-30 03:59:20 +0545

    feat: display user age alongside name in Cards component

--- props/src/components/Cards.jsx
+++ props/src/components/Cards.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 const Cards = (props) => {
-    
   return (
     <>
       <div className="card-container">
         <div className="card-wrapper">
             <img src="https://i.pinimg.com/webp87/736x/5d/36/be/5d36be32f592980f9b25ead5f34e1061.webp" alt="image" />
-            <h1>{props.user}</h1>
+            <h1>{props.user} {props.age}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             <button>View more</button>
         </div>
