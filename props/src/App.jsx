eb70a8dcd53463ed43432f4a888257bc6c344d18
Roshan <pahariroshan55@gmail.com>
2026-06-30 03:38:41 +0545

    feat: initialize props project with React, Vite, and ESLint
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created favicon.svg and icons.svg for application branding.
    - Implemented main application structure in App.jsx and Cards component.
    - Styled application with index.css for layout and card design.
    - Configured Vite for React project in vite.config.js.

---
+++ props/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cards from './components/Cards'
+
+const App = () => {
+  return (
+    <>
+    <div className="parent">
+      <Cards />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ props/src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <>
+      <div className="card-container">
+        <div className="card-wrapper">
+            <img src="https://i.pinimg.com/webp87/736x/5d/36/be/5d36be32f592980f9b25ead5f34e1061.webp" alt="image" />
+            <h1>Roxuwan Roxu</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button>View more</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Cards
