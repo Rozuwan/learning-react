114dd0371ee2a4192d8ecb6eb50f2ae31940e8b6
Roshan <pahariroshan55@gmail.com>
2026-06-30 04:47:39 +0545

    Add initial project setup with components and assets
    - Created Card component for job listings with styling.
    - Added index.css for global styles and card layout.
    - Implemented main entry point with React and Vite configuration.
    - Included SVG assets for React and Vite logos.

---
+++ props-drilling-proj/src/components/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Bookmark } from 'lucide-react';
+const Card = () => {
+  return (
+    <div className='card-container'>
+        
+      <div className="top">
+        <img src="https://images.seeklogo.com/logo-png/40/2/amazon-icon-logo-png_seeklogo-405254.png" alt="" />
+        <button>Save <Bookmark /> </button>
+      </div>
+
+      <div className="center">
+        <h3>Amazon</h3>
+        <h2>Senior UI/UX Designer</h2>
+        <div>
+            <h4>Part time</h4>
+            <h4>Senior Level </h4>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div>
+            <div>
+                <h4>$120/hr</h4>
+                <h4>SanFrancisco,CA</h4>
+            </div>
+            <button>Apply Now</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Card
