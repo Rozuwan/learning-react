06cf812caea947eab143d492b4b76caeca3f4749
Roshan <pahariroshan55@gmail.com>
2026-06-30 03:46:53 +0545

    learnt props passing

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Cards from './components/Cards'
+import React from "react";
+import Cards from "./components/Cards";
 
 const App = () => {
   return (
     <>
-    <div className="parent">
-      <Cards />
-    </div>
+      <div className="parent">
+        <Cards user="Ram Bahadur" age={20}/>
+        <Cards user="Shyam Bahadur" age={29}/>
+        <Cards user="Hari Bahadur" age={30}/>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- props/src/components/Cards.jsx
+++ props/src/components/Cards.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Cards = () => {
+const Cards = (props) => {
+    
   return (
     <>
       <div className="card-container">
         <div className="card-wrapper">
             <img src="https://i.pinimg.com/webp87/736x/5d/36/be/5d36be32f592980f9b25ead5f34e1061.webp" alt="image" />
-            <h1>Roxuwan Roxu</h1>
+            <h1>{props.user}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             <button>View more</button>
         </div>
