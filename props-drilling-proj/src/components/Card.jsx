cb97fca9b39c3be7f79a92288e8cb79208cf99bb
Roshan <pahariroshan55@gmail.com>
2026-07-01 05:27:00 +0545

    fixed

--- props-drilling-proj/src/components/Card.jsx
+++ props-drilling-proj/src/components/Card.jsx
@@ -1,35 +1,35 @@
-import { Bookmark } from 'lucide-react';
+import { Bookmark } from "lucide-react";
 const Card = (props) => {
-  
   return (
-    <div className='card-container'>
-        
+    <div className="card-container">
+      
       <div className="top">
         <img src={props.brandLogo} alt="" />
-        <button>Save <Bookmark /> </button>
+        <button>
+          Save <Bookmark />
+        </button>
       </div>
 
       <div className="center">
         <h3>{props.company}</h3>
         <h2>{props.post}</h2>
-        <div className='center-detail'>
-            <h4>{props.tag1}</h4>
-            <h4>{props.tag2} </h4>
+        <div className="center-detail">
+          <h4>{props.tag1}</h4>
+          <h4>{props.tag2} </h4>
         </div>
       </div>
-     
+
       <div className="bottom">
-            <div>
-                <h4>{props.pay}</h4>
-                <h4>{props.location}</h4>
-            </div>
         <div>
-            <button>Apply Now</button>
+          <h4>{props.pay}</h4>
+          <h4>{props.location}</h4>
+        </div>
+        <div>
+          <button>Apply Now</button>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
