d973eb8b77a7a7df84fe17a77605bb36c2d69eb2
Roshan <pahariroshan55@gmail.com>
2026-07-06 14:30:01 +0545

    refactor: update state management and button functionality in App component

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const [num, setNum] = useState(20)
-  function changeVal() {
-    setNum(50)
+  const [num, setNum] = useState(0);
+  function increase() {
+    setNum(num + 1);
+  }
+  function decrease() {
+    setNum(num - 1);
+  }
+  function jump() {
+    setNum(num + 5);
+  }
+  function reset() {
+    setNum(0);
   }
   return (
     <div>
-      <h1>The value of num is {num}</h1>
-      <button onClick={changeVal}>Click</button>
+      <h1>{num}</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Deacrease</button>
+      <button onClick={jump}>Increase by 5</button>
+      <button onClick={reset}>Reset</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
