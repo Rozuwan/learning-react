78edbf4e27669377a14ec951eaca0dd4f5917294
Roshan <pahariroshan55@gmail.com>
2026-07-06 14:31:48 +0545

    refactor: simplify button click handlers in App component

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,26 +1,42 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  function increase() {
-    setNum(num + 1);
-  }
-  function decrease() {
-    setNum(num - 1);
-  }
-  function jump() {
-    setNum(num + 5);
-  }
-  function reset() {
-    setNum(0);
-  }
+
   return (
     <div>
       <h1>{num}</h1>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Deacrease</button>
-      <button onClick={jump}>Increase by 5</button>
-      <button onClick={reset}>Reset</button>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        onClick={() => {
+          setNum(num - 1);
+        }}
+      >
+        Decrease
+      </button>
+
+      <button
+        onClick={() => {
+          setNum(num + 5);
+        }}
+      >
+        Jump to 5
+      </button>
+
+      <button
+        onClick={() => {
+          setNum(0);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 };
