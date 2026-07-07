765afe82ff72c72a795a0d937a29ed45b33a1efb
Roshan <pahariroshan55@gmail.com>
2026-07-07 11:26:27 +0545

    refactor: streamline form handling and improve input management in App component

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,33 +1,55 @@
+import { useState } from "react";
+
 const App = () => {
-  
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+
+  const submitHandler = (e) => {
+    
+    e.preventDefault();
+    setTitle("");
+    setDetails("");
+    console.log(title, details);
+  };
+
   return (
     <div className="h-screen bg-black lg:flex">
       <form
         onSubmit={(e) => {
-          e.preventDefault();
-
+          submitHandler(e)
         }}
         className="flex flex-col  gap-4  p-10 lg:w-1/2"
-        
       >
         <h1 className="font-bold text-2xl text-white">ADD notes :</h1>
+
         {/* adding input */}
         <input
           className="p-2 border border-white text-white outline-none rounded-lg "
           type="text"
           placeholder="Enter Notes heading"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
 
+        {/* details */}
         <textarea
           className="p-2 border border-white text-white outline-none"
           type="text"
-          placeholder="Enter notes"
+          placeholder="Enter notes here"
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
         />
 
         <button className="bg-white color-black border outline-none rounded-full">
           Add Notes
         </button>
       </form>
+
       <div className=" bg-black p-5 lg:w-1/2">
         <h1 className="font-bold text-2xl text-white">Your notes :</h1>
         <div className="flex flex-wrap h-full overflow-auto gap-5 mt-5">
