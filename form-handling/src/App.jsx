b7919b393bd5805a582fac9d3a3a343e609c03d0
Roshan <pahariroshan55@gmail.com>
2026-07-07 10:01:38 +0545

    feat: implement form handling with state management for title input

--- form-handling/src/App.jsx
+++ form-handling/src/App.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [title, setTitle] = useState("");
+
   return (
     <div>
-      <form onSubmit={(e)=>{
-        e.preventDefault()
-        console.log("submitted");
-        
-      }}>
-        <input type="text" placeholder='Enter'/>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            console.log(e.target.value);
+            
+          }}
+        />
         <button>Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
