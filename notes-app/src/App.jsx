7e2743800d998b5f9a1a9c42ca08710b170e19b7
Roshan <pahariroshan55@gmail.com>
2026-07-07 13:02:06 +0545

    completed

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -3,21 +3,23 @@ import { useState } from "react";
 const App = () => {
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
-
+  const [task, setTask] = useState([]);
 
   const submitHandler = (e) => {
-    
     e.preventDefault();
+    const copytask = [...task];
+    copytask.push({ title, details });
+    setTask(copytask);
     setTitle("");
     setDetails("");
-    console.log(title, details);
+    console.log(copytask);
   };
 
   return (
     <div className="h-screen bg-black lg:flex">
       <form
         onSubmit={(e) => {
-          submitHandler(e)
+          submitHandler(e);
         }}
         className="flex flex-col  gap-4  p-10 lg:w-1/2"
       >
@@ -45,7 +47,7 @@ const App = () => {
           }}
         />
 
-        <button className="bg-white color-black border outline-none rounded-full">
+        <button className="bg-white color-black border outline-none rounded-full active:bg-black active:text-white">
           Add Notes
         </button>
       </form>
@@ -53,9 +55,17 @@ const App = () => {
       <div className=" bg-black p-5 lg:w-1/2">
         <h1 className="font-bold text-2xl text-white">Your notes :</h1>
         <div className="flex flex-wrap h-full overflow-auto gap-5 mt-5">
-          <div className="h-52 w-35 rounded-2xl bg-amber-50"></div>
-          <div className="h-52 w-40 rounded-2xl bg-amber-50"></div>
-          <div className="h-52 w-40 rounded-2xl bg-amber-50"></div>
+          {task.map((e, idx) => {
+            return (
+              <div
+                key={idx}
+                className="h-52 w-35 rounded-2xl bg-amber-50  p-5"
+              >
+                <h2 className="text-xl font-bold">{e.title}</h2>
+                <p className="mt-2 font-medium text-gray-500">{e.details}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
