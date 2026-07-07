156f7081c97cd98a505212c00a8d80f135e81cc9
Roshan <pahariroshan55@gmail.com>
2026-07-08 05:21:40 +0545

    set local storage

--- localstorage/src/App.jsx
+++ localstorage/src/App.jsx
@@ -1,10 +1,12 @@
-
 const App = () => {
+  const user = { name: "roshan", age: 18, city: "lalitpur" };
+  localStorage.setItem('user',JSON.stringify(user))
+  
   return (
     <div>
-      
+      <h1>hey</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
