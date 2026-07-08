730f741a6c090935a15fbafb5429f42f2731f663
Roshan <pahariroshan55@gmail.com>
2026-07-08 13:41:32 +0545

    fetch data from API and display in component

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,15 +1,23 @@
 import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
-  const getData = async()=>{
-   const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-   
-   console.log(response);
-   
-  }
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos",
+    );
+    setData(response.data);
+    
+  };
   return (
     <div>
       <button onClick={getData}>Click</button>
+      <div>
+        {data.map(function (e,idx) {
+          return <h3 >Hello {idx}</h3>
+        })}
+      </div>
     </div>
   );
 };
