e9cc00ebbbcbd91357bbea905476dcaaaa9449c3
Roshan <pahariroshan55@gmail.com>
2026-07-08 13:24:43 +0545

    axios

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,10 +1,12 @@
+import axios from "axios";
+
 const App = () => {
-  const getData = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    const data = await response.json()
-    console.log(data);
-    
-  };
+  const getData = async()=>{
+   const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+   
+   console.log(response);
+   
+  }
   return (
     <div>
       <button onClick={getData}>Click</button>
