035636f1b87799a20b0e87aa312c43f363f914ba
Roshan <pahariroshan55@gmail.com>
2026-07-09 08:46:41 +0545

    Initialize gallery project with React, Vite, and Tailwind CSS; add basic app structure and data fetching functionality

---
+++ gallery-project/src/App.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+  const [userData, setUserData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get(
+      "https://picsum.photos/v2/list?page=1&limit=40",
+    );
+    setUserData(response.data);
+  };
+
+
+  return (
+    <div className="h-screen bg-black text-white p-4 overflow-auto">
+      <button
+        onClick={getData}
+        className="bg-green-600 text-white px-5 py-2 rounded active:scale-95"
+      >
+        get data
+      </button>
+      <div className="">
+        {userData.map((e, idx) => {
+          return (
+            <div key={idx}>
+              <img src={e.download_url} alt="images" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
