92784e9506680af46f22bdfa411c27c9bd804c2e
Roshan <pahariroshan55@gmail.com>
2026-07-09 10:09:49 +0545

    refactor: enhance data fetching and loading state management in App component

--- gallery-project/src/App.jsx
+++ gallery-project/src/App.jsx
@@ -4,27 +4,43 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [userData, setUserData] = useState([]);
   const [index, setIndex] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(
-    function () {
-      const getData = async () => {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+
         const response = await axios.get(
-          `https://picsum.photos/v2/list?page=${index}&limit=20`,
+          `https://picsum.photos/v2/list?page=${index}&limit=20`
         );
+
         setUserData(response.data);
-      };
-      getData();
-    },
-    [index],
-  );
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, [index]);
 
   return (
-    <div className="h-screen bg-black text-white p-4 overflow-auto">
-      <h1>{index}</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 ">
-        {userData.length > 0 ? (
-          userData.map((e, idx) => {
-            return (
+    <div className="min-h-screen bg-black text-white p-4">
+      {loading ? (
+        <div className="h-[80vh] flex justify-center items-center">
+          <button
+            disabled
+            className=" text-white px-8 py-3  font-semibold animate-pulse cursor-not-allowed"
+          >
+            Loading...
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+            {userData.map((e, idx) => (
               <div
                 className="rounded-xl overflow-hidden shadow-lg hover:scale-105 duration-300"
                 key={idx}
@@ -36,34 +52,39 @@ const App = () => {
                   loading="lazy"
                   onClick={() => window.open(e.download_url, "_blank")}
                 />
-                <p className="text-center font-bold text-xl">{e.author}</p>
+
+                <p className="text-center font-bold text-xl py-2">
+                  {e.author}
+                </p>
               </div>
-            );
-          })
-        ) : (
-          <p>No data available</p>
-        )}
-      </div>
-      <div className="flex justify-center items-center gap-6">
-        <button
-          onClick={() => {
-            if (index > 1) {
-              setIndex(index - 1);
-            }
-          }}
-          className="bg-amber-50 text-black px-6  py-2  text-sm  cursor-pointer active:scale-95 font-semibold rounded-2xl"
-        >
-          Prev
-        </button>
-        <button
-          onClick={() => {
-            setIndex(index + 1);
-          }}
-          className="bg-amber-50 text-black px-6  py-2  text-sm  cursor-pointer active:scale-95 font-semibold rounded-2xl"
-        >
-          Nxt
-        </button>
-      </div>
+            ))}
+          </div>
+
+          <div className="flex justify-center items-center gap-6 pb-5">
+            <button
+              onClick={() => {
+                if (index > 1) {
+                  setIndex((prev) => prev - 1);
+                }
+              }}
+              className="bg-amber-50 text-black px-6 py-2 text-sm cursor-pointer active:scale-95 font-semibold rounded-2xl"
+            >
+              Prev
+            </button>
+
+            <span className="text-xl font-bold">{index}</span>
+
+            <button
+              onClick={() => {
+                setIndex((prev) => prev + 1);
+              }}
+              className="bg-amber-50 text-black px-6 py-2 text-sm cursor-pointer active:scale-95 font-semibold rounded-2xl"
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
