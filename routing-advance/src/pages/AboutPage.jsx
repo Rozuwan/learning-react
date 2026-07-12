5a456556a7c1653adb7db5e2c37568adfba87ec9
Roshan <pahariroshan55@gmail.com>
2026-07-12 16:01:10 +0545

    use navigate

--- routing-advance/src/pages/AboutPage.jsx
+++ routing-advance/src/pages/AboutPage.jsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
 
 const AboutPage = () => {
+  const navigate = useNavigate();
+  const btnClick = () => {
+    navigate("/");
+  };
   return (
     <div>
       <h1>About page</h1>
+      <button
+        onClick={btnClick}
+        className="cursor-pointer bg-amber-500 rounded-full px-2 active:scale-95"
+      >
+        Return to homepage
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
