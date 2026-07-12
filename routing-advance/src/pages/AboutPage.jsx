6dcc820cee482d5be24a97e7c2fa2176f00b97c9
Roshan <pahariroshan55@gmail.com>
2026-07-12 18:59:19 +0545

    refactor: simplify button click handlers in AboutPage component

--- routing-advance/src/pages/AboutPage.jsx
+++ routing-advance/src/pages/AboutPage.jsx
@@ -2,17 +2,24 @@ import { useNavigate } from "react-router-dom";
 
 const AboutPage = () => {
   const navigate = useNavigate();
-  const btnClick = () => {
-    navigate("/");
-  };
   return (
     <div>
       <h1>About page</h1>
       <button
-        onClick={btnClick}
+        onClick={() => {
+          navigate('/')
+        }}
         className="cursor-pointer bg-amber-500 rounded-full px-2 active:scale-95"
       >
         Return to homepage
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1)
+        }}
+        className="cursor-pointer bg-amber-500 rounded-full px-2 active:scale-95"
+      >
+       Back
       </button>
     </div>
   );
